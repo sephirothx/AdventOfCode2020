d0aa2553985d100faaf474d124071d809ab91dbe
Stefano Prosperi <step.prosperi@gmail.com>
2020-12-01 06:23:45 +0100

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,16 @@
+inp = list(map(int, open('input.txt').read().splitlines()))
+inp.sort()
+
+for idx, i in enumerate(inp):
+    lo = idx + 1
+    hi = len(inp) - 1
+    while lo < hi:
+        s = i + inp[lo] + inp[hi]
+        p = i * inp[lo] * inp[hi]
+        if s == 2020:
+            print(p)
+            exit()
+        if s < 2020:
+            lo = lo + 1
+        if s > 2020:
+            hi = hi - 1
