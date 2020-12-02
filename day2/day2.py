6e14b36bc1d80f8a2c9fb75d9cd5a6d4cb3f27e7
Stefano Prosperi <step.prosperi@gmail.com>
2020-12-02 23:20:42 +0100

    day 2: general improvements

--- day2/day2.py
+++ day2/day2.py
@@ -5,21 +5,15 @@ inp = open('input.txt').readlines()
 sol1 = 0
 sol2 = 0
 for s in inp:
-    m = re.match(r'(\d+)-(\d+) (\w): (\w+)', s)
-    lo = int(m.group(1))
-    hi = int(m.group(2))
-    lett = m.group(3)
-    word = m.group(4)
+    lo, hi, c, word = re.findall(r'(\d+)-(\d+) (\w): (\w+)', s)[0]
+    lo = int(lo)
+    hi = int(hi)
 
-    occ = 0
-    for c in word:
-        if c == lett:
-            occ += 1
-    if lo <= occ <= hi:
+    if lo <= word.count(c) <= hi:
         sol1 += 1
 
-    cond1 = word[lo-1] == lett
-    cond2 = word[hi-1] == lett
+    cond1 = word[lo-1] == c
+    cond2 = word[hi-1] == c
     if cond1 ^ cond2:
         sol2 += 1
 
