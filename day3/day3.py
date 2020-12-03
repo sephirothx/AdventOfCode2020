88017fc03414e3b2b92466111fa93f936fe2cbdf
Stefano Prosperi <step.prosperi@gmail.com>
2020-12-03 10:19:04 +0100

    day 3: generalized solution

--- day3/day3.py
+++ day3/day3.py
@@ -1,11 +1,18 @@
 inp = open('input.txt').readlines()
 
-pos = 0
-sol = 0
-for s in inp:
-    s = s.strip()  # apparently readlines does not strip :(
-    if s[pos % len(s)] == '#':
-        sol += 1
-    pos += 3
+def count(dx, dy):
+    x = 0
+    ret = 0
+    for i in range(0, len(inp), dy):
+        s = inp[i].strip()  # apparently readlines does not strip :(
+        if s[x % len(s)] == '#':
+            ret += 1
+        x += dx
+    return ret
 
-print(sol)
+print(count(3,1))
+print(count(1,1) *
+      count(3,1) *
+      count(5,1) *
+      count(7,1) *
+      count(1,2))
